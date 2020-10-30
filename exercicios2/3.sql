9f6b306ccd36cd997947a9f065f8f97684d78502
franciscosegui <67435469+franciscosegui@users.noreply.github.com>
2020-10-30 13:13:51 -0300

    3.sql

---
+++ exercicios2/3.sql
@@ -0,0 +1,4 @@
+SELECT first_name, last_name, dept_name
+FROM employees LEFT JOIN dept_manager ON employees.emp_no = dept_manager.emp_no 
+INNER JOIN departments ON dept_manager.dept_no = departments.dept_no
+WHERE employees.emp_no IN (110022,110085,10006)
