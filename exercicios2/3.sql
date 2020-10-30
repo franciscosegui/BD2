1cf36c8e14ba7b91b0426181f69b2ec84e882af1
franciscosegui <67435469+franciscosegui@users.noreply.github.com>
2020-10-30 13:15:31 -0300

    Update 3.sql

--- exercicios2/3.sql
+++ exercicios2/3.sql
@@ -2,3 +2,12 @@ SELECT first_name, last_name, dept_name
 FROM employees LEFT JOIN dept_manager ON employees.emp_no = dept_manager.emp_no 
 INNER JOIN departments ON dept_manager.dept_no = departments.dept_no
 WHERE employees.emp_no IN (110022,110085,10006)
+
+
+OR
+
+
+SELECT first_name, last_name, dept_name
+FROM employees LEFT JOIN dept_manager ON employees.emp_no = dept_manager.emp_no 
+LEFT JOIN departments ON dept_manager.dept_no = departments.dept_no
+WHERE employees.emp_no IN (110022,110085,10006)
